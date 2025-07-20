1043599034944f4f8222da17c1bbc2e75eedd615
lightyyt <lightmagkuchen@gmail.com>
2025-07-20 10:40:13 +0200

    Big Update - Fixes, Project Page, Recolor

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
 function redir(site) {
   let sites = [
+    "/",
     "https://youtube.com/@LightyCode",
     "https://bsky.app/profile/lightyyt.bsky.social",
-    "https://discord.gg/dzmap8Sgsv"
+    "https://discord.gg/dzmap8Sgsv",
+    "https://github.com/lightyyt/Chuckle"
   ]
   let red = sites[site];
   window.location = red;
+}
+// Theming
+
+function darkmodeText(isDark) {
+  // Set Dark mode element text
+  document.getElementById("darkmode").innerHTML = isDark ? "Dark Mode 🌙" : "Light Mode ☀️";
+}
+// Toggle and remember theme
+function toggleTheme() {
+  const html = document.documentElement;
+  const isDark = html.getAttribute("data-theme") == "dark";
+  html.setAttribute("data-theme", isDark ? "light" : "dark");
+  localStorage.setItem("theme", isDark ? "light" : "dark");
+  
+  darkmodeText(isDark);
+}
+
+// Load saved theme on page load
+window.addEventListener("DOMContentLoaded", () => {
+  const html = document.documentElement;
+  
+  html.classList.add("no-transition"); // Disable transitions
+  const saved = localStorage.getItem("theme");
+  if (saved) document.documentElement.setAttribute("data-theme", saved);
+  
+  html.classList.remove("no-transition")
+  
+  darkmodeText(saved == "light");
+});
+
+let menuOpen = false;
+function hamburger() {
+  menuOpen = !menuOpen;
+  document.getElementById("menu").style.width = menuOpen ? "13rem" : "0rem";
+}
+
+
+function intern(web) {
+  window.location = web;
 }
---
+++ js/project.js
@@ -0,0 +1,46 @@
+
+  class ProjectCard extends HTMLElement {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+      const img = this.getAttribute('img') || '';
+      const info = this.getAttribute('info') || '';
+      const desc = this.getAttribute('desc') || '';
+
+      const myhead = document.createElement('head');
+      myhead.innerHTML = `
+  <link rel="stylesheet" href="/css/pfp.css">
+  <link rel="stylesheet" href="/css/index.css">
+  <link rel="stylesheet" href="/css/main.css">
+  <link rel="stylesheet" href="/css/cen_h1.css">
+  <link rel="stylesheet" href="/css/project.css">
+      `;
+      const wrapper = document.createElement('span');
+      
+      wrapper.classList.add('project');
+      if(img == "noimg"){
+        wrapper.innerHTML = `
+        <span class="left">
+          <img src="" style="visibility: hidden;" class="smallimg" />`
+      } else {
+        wrapper.innerHTML = `
+        <span class="left">
+          <img src="${img}" class="smallimg" />`
+      }
+      wrapper.innerHTML +=`
+        </span>
+        <div class="main-info" style="flex-direction:column;">
+          <div class="title">${info}</div>
+          <div class="desc">${desc}</div>
+        </div>
+      `;
+
+      this.shadowRoot.appendChild(myhead);
+      this.shadowRoot.appendChild(wrapper);
+    }
+  }
+
+  customElements.define('project-card', ProjectCard);
